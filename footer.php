cc16aaae6f9281e3c737d2183546f394ddfa52c7
Elizabeth Barnum <elizabethbarnum@Elizabeths-MacBook-Air.local>
2023-06-04 09:29:04 -0600

    updated images to new logo

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 	<div class="slide-inner">
 		<a href="<?php echo $host; ?>">Home</a> <!-- Put your links to other pages here --> <a href="<?php echo $host; ?>privacy/" target="_blank">Privacy Policy</a>
 		<p class="copyright">&copy; 2023 Yourdomainame.com</p><!-- IMPORTANT: Put your domain name here -->
+		<a href="https://www.flaticon.com/free-icons/bmi" title="bmi icons">Bmi icons created by DailyPm Studio - Flaticon</a>
 	</div>
 	<!-- PUT YOUR GOOGLE ANALYTICS CODE SNIPPET HERE -->
 </div>
