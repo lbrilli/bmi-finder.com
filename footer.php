31e7c0448340a729b2ac1ebe9ed47707a124a64d
Elizabeth Barnum <elizabethbarnum@Elizabeths-MacBook-Air.local>
2023-06-04 12:49:55 -0600

    small footer update

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 	<div class="slide-inner">
 		<a href="<?php echo $host; ?>">Home</a> <!-- Put your links to other pages here --> <a href="<?php echo $host; ?>privacy/" target="_blank">Privacy Policy</a>
-		<p class="copyright">&copy; 2023 Yourdomainame.com</p><!-- IMPORTANT: Put your domain name here -->
+		<p class="copyright">&copy; 2023 bmi-finder.com</p><!-- IMPORTANT: Put your domain name here -->
 		<a href="https://www.flaticon.com/free-icons/bmi" title="bmi icons">Bmi icons created by DailyPm Studio - Flaticon</a>
 	</div>
 	<!-- Google tag (gtag.js) -->
