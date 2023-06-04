3a1c61a4bc0fe8a49553b98fd9183838e501c480
Elizabeth Barnum <elizabethbarnum@Elizabeths-MacBook-Air.local>
2023-06-04 10:24:31 -0600

    icons updated and tool in metric units should be done

--- index.php
+++ index.php
@@ -197,7 +197,56 @@
 					</div>
 					<div id="tool">
 						<div id="control-panel">
-							<!-- THIS IS WHERE YOUR CUSTOM CODE FOR YOUR TOOL GOES! E.g. a calculator, jokes, quotes, generators, whatever you are building goes here -->
+							<script>
+								window.onload = () => {
+								let button = document.querySelector("#btn");
+							
+								// Function for calculating BMI
+								button.addEventListener("click", calculateBMI);
+								};
+								
+								function calculateBMI() {
+								
+									/* Getting input from user into height variable.
+									Input is string so typecasting is necessary. */
+									let height = parseInt(document
+											.querySelector("#height").value);
+								
+									/* Getting input from user into weight variable. 
+									Input is string so typecasting is necessary.*/
+									let weight = parseInt(document
+											.querySelector("#weight").value);
+								
+									let result = document.querySelector("#result");
+								
+									// Checking the user providing a proper
+									// value or not
+									if (height === "" || isNaN(height)) 
+										result.innerHTML = "Provide a valid Height!";
+								
+									else if (weight === "" || isNaN(weight)) 
+										result.innerHTML = "Provide a valid Weight!";
+								
+									// If both input is valid, calculate the bmi
+									else {
+								
+										// Fixing upto 2 decimal places
+										let bmi = (weight / ((height * height) 
+															/ 10000)).toFixed(2);
+								
+										// Dividing as per the bmi conditions
+										if (bmi < 18.6) result.innerHTML =
+											`Under Weight : <span>${bmi}</span>`;
+								
+										else if (bmi >= 18.6 && bmi < 24.9) 
+											result.innerHTML = 
+												`Normal : <span>${bmi}</span>`;
+								
+										else result.innerHTML =
+											`Over Weight : <span>${bmi}</span>`;
+									}
+								}
+							</script>
 						</div>
 						<div class="bottomad">					
 							<!-- This is where your 320x50 bottom ad code from Adsense goes -->
@@ -205,6 +254,7 @@
 					</div>
 				</div>
 				<div class="content-inner">
+					<!-- This is where we can place supporting content about the tool, FAQ etc. -->
 					<div class="section minor">
 						<h3 class="subtitle-minor">About XYZ</h3>
 						<p class="subtext-minor">About section text to describe your tool...</p>
