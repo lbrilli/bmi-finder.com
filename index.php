70c81a280fecce248c0b4571bd66b6539eb46043
Elizabeth Barnum <elizabethbarnum@Elizabeths-MacBook-Air.local>
2023-06-06 18:57:19 -0600

    added content to the page

--- index.php
+++ index.php
@@ -300,23 +300,43 @@
 					<!-- This is where we can place supporting content about the tool, FAQ etc. -->
 					<div class="section minor">
 						<h3 class="subtitle-minor">About BMI Finder</h3>
-						<p class="subtext-minor">Calculate your Body Mass Index using your height and weight using this free online calculator. Find our your BMI using either imperial or metric measurements.</p>
+						<p class="subtext-minor">Calculate your Body Mass Index using your height and weight using this free online 
+							calculator. Find our your BMI using either imperial or metric measurements. BMI (Body Mass Index) is a 
+							simple calculation that uses a person's height and weight to assess their weight status. It does not 
+							measure body fat directly, but it is a good indicator of where someone may stand.						
+						</p>
 					</div>
 					<hr/>
 					<div class="section minor">
-						<h3 class="subtitle-minor">Questions about BMI Finder</h3>
-						<p class="subtext-minor">Questions</p>
-					</div>
-					<hr/>
-					<div class="section minor">
-						<h3 class="subtitle-minor">Another section about XYZ</h3>
-						<p class="subtext-minor">More text about XYZ</p>
+						<h3 class="subtitle-minor">What can I use my BMI for?</h3>
+						<br>
 						<ul>
-							<li>Test</li>
-							<li>Option 2</li>
-							<li>Option 3</li>
+							<li>To screen for weight-related health risks. A high BMI can be a sign of increased risk for chronic 
+								diseases such as heart disease, stroke, type 2 diabetes, and some types of cancer.</li>
+							<li>To track weight loss or gain over time. BMI can be used to track changes in weight status over time, 
+								which can help people stay motivated in their weight loss or weight gain goals.</li>
+							<li>To compare weight status to population norms. BMI can be used to compare a person's weight status to 
+								population norms, which can help people understand where they fall in terms of healthy weight.</li>
+							<li>It is important to note that BMI is not a perfect measure of body fatness. It does not take into 
+								account muscle mass, bone density, or body composition. Therefore, it is important to talk to your 
+								doctor about your weight status and any concerns you may have.</li>
 						</ul>
-						<p class="subtext-minor">Even more text about XYZ</p>
+					</div>
+					<hr/>
+					<div class="section minor">
+						<h3 class="subtitle-minor">What can I use BMI Finder for?</h3>
+						<ul>
+							<li>To determine your weight category. BMI is classified into four categories: underweight, normal weight, 
+								overweight, and obese. Your weight category can help you understand your risk for health problems 
+								related to weight.</li>
+							<li>To track your weight over time. BMI can be used to track changes in weight over time, which can help you 
+								stay motivated in your weight loss or weight gain goals.</li>
+							<li>To compare your weight to the average weight for your age and sex. BMI can be used to compare your weight 
+								to the average weight for your age and sex, which can help you understand if your weight is within a 
+								healthy range.</li>
+							<li>If you are concerned about your weight or weight status, talk to your doctor. They can help you 
+								understand your BMI and what it means for your health.</li>
+						</ul>
 					</div>
 				</div>
 			</div>
