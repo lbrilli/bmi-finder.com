471e82f363e80caa5b4a741bf4db88b819d8de61
lbrilli11 <lbrilli11@gmail.com>
2024-01-31 19:27:58 +0000

    fix nav bar pp

--- nav.php
+++ nav.php
@@ -4,11 +4,12 @@
 		<div class="nav-header">Menu</div>
 		<a class="nav-item" href="<?php echo __DIR__; ?>">Home</a>
 		<div class="nav-divider"></div>
-		<!-- Put your links to other pages here -->
-		<div class="nav-header">Privacy &amp; Terms</div>
-		<a class="nav-item" href="<?php echo $host; ?>privacy/">Privacy Policy</a>
 		<div class="nav-header">Limitations of BMI</div>
-		<a class="nav-item" href="<?php echo $host; ?>limitations/" target="_blank">Limitations of BMI</a>
+		<a class="nav-item" href="<?php echo $host; ?>limitations/">Limitations of BMI</a>
+		<div class="nav-header">Privacy &amp; Terms</div>
+		<a class="nav-item" href="<?php echo $host; ?>privacy/" target="_blank">Privacy Policy</a>
+		<div class="nav-header">Limitations of BMI</div>
+		<a class="nav-item" href="<?php echo $host; ?>limitations/">Limitations of BMI</a>
 		<div class="nav-divider"></div>
 	</div>
 </div>
