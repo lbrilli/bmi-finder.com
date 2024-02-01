23bbc0c69b498b03460f053df40981bc033124db
lbrilli11 <lbrilli11@gmail.com>
2024-02-01 00:49:56 +0000

    update nav bar

--- nav.php
+++ nav.php
@@ -4,10 +4,8 @@
 		<div class="nav-header">Menu</div>
 		<a class="nav-item" href="<?php echo __DIR__; ?>">Home</a>
 		<div class="nav-divider"></div>
-		<!-- <div class="nav-header">Limitations of BMI</div> -->
 		<a class="nav-item" href="<?php echo $host; ?>limitations/">Limitations of BMI</a>
 		<div class="nav-divider"></div>
-		<!-- <div class="nav-header">Privacy &amp; Terms</div> -->
 		<a class="nav-item" href="<?php echo $host; ?>privacy/" target="_blank">Privacy Policy</a>
 		<div class="nav-divider"></div>
 	</div>
