402ca54dc94fc1ec69a6680d26ccf6648029a203
lbrilli11 <lbrilli11@gmail.com>
2024-02-04 04:28:42 +0000

    adding myths page and links

--- nav.php
+++ nav.php
@@ -6,6 +6,8 @@
 		<div class="nav-divider"></div>
 		<a class="nav-item" href="<?php echo $host; ?>interpretation/">Interpreting BMI results</a>
 		<div class="nav-divider"></div>
+		<a class="nav-item" href="<?php echo $host; ?>myths/">BMI: Beyond the Myths</a>
+		<div class="nav-divider"></div>
 		<a class="nav-item" href="<?php echo $host; ?>limitations/">Limitations of BMI</a>
 		<div class="nav-divider"></div>
 		<a class="nav-item" href="<?php echo $host; ?>privacy/" target="_blank">Privacy Policy</a>
